0b50a3f39616da503aa847808cc971eae41b4777
ediaz13 <diazemanuel27@gmail.com>
2024-08-22 18:53:43 -0300

    Fix

--- 3_recursivity/LookForKey.py
+++ 3_recursivity/LookForKey.py
@@ -6,12 +6,12 @@ def look_for_key(main_box):
             if item in box:
                 pile.append(item)
             elif item.is_a_key():
-                print "Found the key!"
+                print("Found the key!")
 
 def look_for_key(box):
     for item in box:
         if item.is_a_box():
             look_for_key(item)
         elif item.is_a_key():
-            print "Found the Key!"
+            print ("Found the Key!")
 
